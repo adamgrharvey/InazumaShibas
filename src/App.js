129f6a40e8ac3a97acf94c6a257be87e374d79cf
adamgrharvey <adamgrharvey@gmail.com>
2023-05-30 11:39:07 -0700

    Adds Router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <button className='button'>Home</button>
+        <button className='button'>History</button>
+        <button className='button'>Males</button>
+        <button className='button'>Females</button>
+        <button className='button'>Litters</button>
+        <button className='button'>Handling</button>
+      </div>
+    </BrowserRouter>
   );
 }
 
