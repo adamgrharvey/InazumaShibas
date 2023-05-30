81c52c10492db7f30470fc122a2a2f594ffe3514
adamgrharvey <adamgrharvey@gmail.com>
2023-05-30 11:56:36 -0700

    Tailwind fixes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
+import "./index.css";
+import Home from './routes/Home';
+
 
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <button className='button'>Home</button>
-        <button className='button'>History</button>
-        <button className='button'>Males</button>
-        <button className='button'>Females</button>
-        <button className='button'>Litters</button>
-        <button className='button'>Handling</button>
+        <div className='Nav-group'>
+          <button className='button'>Home</button>
+          <button className='button'>History</button>
+          <button className='button'>Males</button>
+          <button className='button'>Females</button>
+          <button className='button'>Litters</button>
+          <button className='button'>Handling</button>
+        </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
       </div>
     </BrowserRouter>
   );
