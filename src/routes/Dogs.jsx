25539a685d12162d14a7f9c5ef11ccd5b330bfe0
adamgrharvey <adamgrharvey@gmail.com>
2023-05-31 13:19:58 -0700

    close to final DOGS page

--- src/routes/Dogs.jsx
+++ src/routes/Dogs.jsx
@@ -1,5 +1,9 @@
 import '../CSS/Dogs.css';
 import ayaprofile from "../images/aya-profile-crop.webp";
+import aya1 from "../images/aya1.webp"
+import aya2 from "../images/aya2.webp"
+import aya3 from "../images/aya3.webp"
+import aya4 from "../images/aya4.webp"
 
 import React, { useRef, useState } from "react";
 // Import Swiper React components
@@ -8,11 +12,13 @@ import { Swiper, SwiperSlide } from "swiper/react";
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/free-mode";
-import "swiper/css/navigation";
 import "swiper/css/thumbs";
+import "swiper/css/zoom";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
 // import required modules
-import { FreeMode, Navigation, Thumbs } from "swiper";
+import { FreeMode, Pagination, Zoom, Navigation, Thumbs } from "swiper";
 
 export default function Dogs(props) {
 
@@ -31,28 +37,26 @@ export default function Dogs(props) {
           <p className="mt-4">Aya finished her Championship in only one month of showing with group placings. She went on to attain Grand Champion status at 18 months old.</p>
         </div>
       </div>
-      <div className='AyaPicSwiper'>
+      <div className='mt-5 AyaPicSwiper'>
         <Swiper
           style={{
             "--swiper-navigation-color": "#fff",
             "--swiper-pagination-color": "#fff",
           }}
           loop={true}
+          lazy={true}
           spaceBetween={10}
-          slidesPerView={'auto'}
+          slidesPerView={1}
           navigation={true}
           thumbs={{ swiper: thumbsSwiper && !thumbsSwiper.destroyed ? thumbsSwiper : null }}
-          modules={[FreeMode, Navigation, Thumbs]}
+          modules={[Pagination, FreeMode, Navigation, Thumbs]}
           className="mySwiper2"
         >
           <SwiperSlide>
             <img src="https://img1.wsimg.com/isteam/ip/44dbcf5e-8c9c-4211-84cb-7a2f02434cac/231020437_10162461944164616_393692553189869963.jpg/:/rs=w:719,h:959" />
           </SwiperSlide>
           <SwiperSlide>
-            <img src="https://img1.wsimg.com/isteam/ip/44dbcf5e-8c9c-4211-84cb-7a2f02434cac/230969451_10162461943944616_5679006132831-0001.jpg/:/rs=w:719,h:959" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src="https://img1.wsimg.com/isteam/ip/44dbcf5e-8c9c-4211-84cb-7a2f02434cac/232040672_10162461943244616_586775496065909540.jpg/:/rs=w:719,h:959" />
+            <img src={aya1} />
           </SwiperSlide>
           <SwiperSlide>
             <img src="https://img1.wsimg.com/isteam/ip/44dbcf5e-8c9c-4211-84cb-7a2f02434cac/IMG_6643.jpg/:/rs=w:1280,h:854" />
@@ -70,30 +74,28 @@ export default function Dogs(props) {
             <img src="https://img1.wsimg.com/isteam/ip/44dbcf5e-8c9c-4211-84cb-7a2f02434cac/115892600_10161246907424616_498738289950457989.jpg/:/rs=w:719,h:959" />
           </SwiperSlide>
           <SwiperSlide>
-            <img src="https://img1.wsimg.com/isteam/ip/44dbcf5e-8c9c-4211-84cb-7a2f02434cac/116156717_10161246883489616_315456234594415175.jpg/:/cr=t:0%25,l:0%25,w:100%25,h:100%25/rs=w:719,h:959" />
+            <img src={aya3} />
           </SwiperSlide>
           <SwiperSlide>
-            <img src="https://img1.wsimg.com/isteam/ip/44dbcf5e-8c9c-4211-84cb-7a2f02434cac/108995073_10161246907004616_971498797336438251.jpg/:/rs=w:719,h:959" />
+            <img src={aya2} />
           </SwiperSlide>
         </Swiper>
         <Swiper
           onSwiper={setThumbsSwiper}
           loop={true}
+          lazy={true}
           spaceBetween={10}
-          slidesPerView={6}
+          slidesPerView={'auto'}
           freeMode={true}
           watchSlidesProgress={true}
           modules={[FreeMode, Navigation, Thumbs]}
           className="mySwiper"
         >
-                    <SwiperSlide>
+          <SwiperSlide>
             <img src="https://img1.wsimg.com/isteam/ip/44dbcf5e-8c9c-4211-84cb-7a2f02434cac/231020437_10162461944164616_393692553189869963.jpg/:/rs=w:719,h:959" />
           </SwiperSlide>
           <SwiperSlide>
-            <img src="https://img1.wsimg.com/isteam/ip/44dbcf5e-8c9c-4211-84cb-7a2f02434cac/230969451_10162461943944616_5679006132831-0001.jpg/:/rs=w:719,h:959" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src="https://img1.wsimg.com/isteam/ip/44dbcf5e-8c9c-4211-84cb-7a2f02434cac/232040672_10162461943244616_586775496065909540.jpg/:/rs=w:719,h:959" />
+            <img src={aya1} />
           </SwiperSlide>
           <SwiperSlide>
             <img src="https://img1.wsimg.com/isteam/ip/44dbcf5e-8c9c-4211-84cb-7a2f02434cac/IMG_6643.jpg/:/rs=w:1280,h:854" />
@@ -111,10 +113,10 @@ export default function Dogs(props) {
             <img src="https://img1.wsimg.com/isteam/ip/44dbcf5e-8c9c-4211-84cb-7a2f02434cac/115892600_10161246907424616_498738289950457989.jpg/:/rs=w:719,h:959" />
           </SwiperSlide>
           <SwiperSlide>
-            <img src="https://img1.wsimg.com/isteam/ip/44dbcf5e-8c9c-4211-84cb-7a2f02434cac/116156717_10161246883489616_315456234594415175.jpg/:/cr=t:0%25,l:0%25,w:100%25,h:100%25/rs=w:719,h:959" />
+            <img src={aya3} />
           </SwiperSlide>
           <SwiperSlide>
-            <img src="https://img1.wsimg.com/isteam/ip/44dbcf5e-8c9c-4211-84cb-7a2f02434cac/108995073_10161246907004616_971498797336438251.jpg/:/rs=w:719,h:959" />
+            <img src={aya2} />
           </SwiperSlide>
         </Swiper>
       </div>
