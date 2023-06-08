2942674ebd2b69470fde9c740623f8edffed2868
adamgrharvey <adamgrharvey@gmail.com>
2023-06-08 00:16:02 -0700

    Swiper, dogs page styling

--- src/routes/Dogs.jsx
+++ src/routes/Dogs.jsx
@@ -18,7 +18,7 @@ import "swiper/css/navigation";
 import "swiper/css/pagination";
 
 // import required modules
-import { FreeMode, Pagination, Zoom, Navigation, Thumbs } from "swiper";
+import { Autoplay, FreeMode, Pagination, Zoom, Navigation, Thumbs } from "swiper";
 
 export default function Dogs(props) {
 
@@ -31,10 +31,10 @@ export default function Dogs(props) {
           <img className='rounded-full AyaProfile' src={ayaprofile}></img>
         </div>
         <div className="ml-3 mr-3">
-          <p className='font-bold'>Aya (GCH Sunojo's Kaika Suru Ayame)</p>
-          <p className='mt-4'>Our lovely girl comes from a long line of Shiba Inu's that were imported from Japan. The co-owner of this beautiful girl, Susan Norris-Jones from Sunojo kennels, has been breeding Shiba's since 1985. We are extremely fortunate to have Susan as our mentor in this remarkable breed.</p>
-          <p className="mt-4">Aya is a very elegant and spunky girl. She loves learning new tricks and is eager to impress, however her greatest joy in life is to argue back and to make sure her opinion is heard. She is our foundation bitch and we can't wait to see what she holds for us in the future.</p>
-          <p className="mt-4">Aya finished her Championship in only one month of showing with group placings. She went on to attain Grand Champion status at 18 months old.</p>
+          <p className='font-bold text-2xl'>Aya (GCH Sunojo's Kaika Suru Ayame)</p>
+          <p className='mt-4 text-lg'>Our lovely girl comes from a long line of Shiba Inu's that were imported from Japan. The co-owner of this beautiful girl, Susan Norris-Jones from Sunojo kennels, has been breeding Shiba's since 1985. We are extremely fortunate to have Susan as our mentor in this remarkable breed.</p>
+          <p className="mt-4 text-lg">Aya is a very elegant and spunky girl. She loves learning new tricks and is eager to impress, however her greatest joy in life is to argue back and to make sure her opinion is heard. She is our foundation bitch and we can't wait to see what she holds for us in the future.</p>
+          <p className="mt-4 text-lg">Aya finished her Championship in only one month of showing with group placings. She went on to attain Grand Champion status at 18 months old.</p>
         </div>
       </div>
       <div className='mt-5 AyaPicSwiper'>
@@ -44,18 +44,21 @@ export default function Dogs(props) {
             "--swiper-pagination-color": "#fff",
           }}
           loop={true}
+          autoplay={{
+            delay: 3500,
+            disableOnInteraction: false,
+          }}
           lazy={true}
           spaceBetween={10}
           slidesPerView={1}
           navigation={true}
           thumbs={{ swiper: thumbsSwiper && !thumbsSwiper.destroyed ? thumbsSwiper : null }}
-          modules={[Pagination, FreeMode, Navigation, Thumbs]}
+          modules={[Autoplay, Pagination, FreeMode, Navigation, Thumbs]}
           className="mySwiper2"
         >
           <SwiperSlide>
             <img src="https://img1.wsimg.com/isteam/ip/44dbcf5e-8c9c-4211-84cb-7a2f02434cac/IMG_6643.jpg/:/rs=w:1280,h:854" />
           </SwiperSlide>
-
 
           <SwiperSlide>
             <img src="https://img1.wsimg.com/isteam/ip/44dbcf5e-8c9c-4211-84cb-7a2f02434cac/IMG_6630.jpg/:/rs=w:1280,h:854" />
