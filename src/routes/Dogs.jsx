6085596a6a1c41f3c4bde5daa4d8a5b74843f86e
Adam Harvey <adamgrharvey@gmail.com>
2024-02-25 19:41:35 -0800

    Update Dogs.jsx

--- src/routes/Dogs.jsx
+++ src/routes/Dogs.jsx
@@ -73,9 +73,10 @@ export default function Dogs(props) {
           </div>
           <div className="ml-3 mr-3">
             <p className='font-bold text-2xl'>Aya (GCh Sunojo's Kaika Suru Ayame)</p>
-            <p className='mt-4 text-lg'>Our lovely girl comes from a long line of Shiba Inu's that were imported from Japan. The co-owner of this beautiful girl, Susan Norris-Jones from Sunojo kennels, has been breeding Shiba's since 1985. We are extremely fortunate to have Susan as our mentor in this remarkable breed.</p>
             <p className="mt-4 text-lg">Aya is a very elegant and spunky girl. She loves learning new tricks and is eager to impress, however her greatest joy in life is to argue back and to make sure her opinion is heard. She is our foundation bitch and we can't wait to see what she holds for us in the future.</p>
             <p className="mt-4 text-lg">Aya finished her Championship in only one month of showing with group placings. She went on to attain Grand Champion status at 18 months old.</p>
+            <p className='mt-4 text-lg'>The co-owner of this beautiful girl, Susan Norris-Jones from Sunojo kennels, has been breeding Shiba's since 1985. We are extremely fortunate to have Susan as our mentor in this remarkable breed.</p>
+
           </div>
         </div>
         <div className='mt-5 AyaPicSwiper fadeInPage'>
