9edd3eb578fb4c1e8a6b2ffa7d7d5ae546615e91
Adam <adamgrharvey@gmail.com>
2024-07-08 20:21:51 -0700

    Remove unused import in App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 import './index.css'
 import Home from './routes/Home'
